7b59702be88e66be5cb926cf3c3de4fd139e793f
Alienworm <1370582154@qq.com>
2024-10-08 18:26:35 +0800

    init
    v1

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# PD-BAR
+# PD-BA&R
+## 1. Requirements
+- Python 3.7
+- torch 1.13.1+cu171
+- torch_geometric 2.2.0
+- numpy 1.21.6
+- pandas 1.1.5
+- sqlite3 2.6.0
+## 2. Datasets
+- raw_data/Steam
+- raw_data/Youshu
+- raw_data/NetEase
+## 3. Scripts
+- get_args.py: parse arguments(with default values) from command line
+- run.sh: run the model with specified arguments
+## 4. Example of running the model on Steam dataset
+```python
+./run.sh --dataset_name Steam --tr "(8,20)" --ars "[(7,7)]" --an 2 --max_sample_num_per_user 1 --augment_batch_size 256 --train_batch_size 256 --max_ub_num_quantile 1.0 --max_ui_num_quantile 1.0 --max_bi_num_quantile 1.0 --early_stop 20 --embedding_dim 64 --eval_interval 5 --lightgcn_layers 5 --noise_scale 0.2
+```
---
+++ run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+# shellcheck disable=SC2154
+
+source get_args.sh
+
+# if is_augment is True, run user-bundle data augmentation
+if [ "$is_augment" = "True" ]; then
+  # user-bundle data augmentation
+  python main.py \
+    --dataset_name "$dataset_name" \
+    --dataset_root_path "$dataset_root_path" \
+    --tr "$tr" \
+    --ars "$ars" \
+    --an "$an" \
+    --max_sample_num_per_user "$max_sample_num_per_user" \
+    --max_ub_num_quantile "$max_ub_num_quantile" \
+    --max_ui_num_quantile "$max_ui_num_quantile" \
+    --max_bi_num_quantile "$max_bi_num_quantile" \
+    --lightgcn_layers "$lightgcn_layers" \
+    --noise_schedule "$noise_schedule" \
+    --noise_scale "$noise_scale" \
+    --min_noise "$min_noise" \
+    --max_noise "$max_noise" \
+    --max_diffusion_steps "$max_diffusion_steps" \
+    --embedding_dim "$embedding_dim" \
+    --device "$device" \
+    --train_batch_size "$train_batch_size" \
+    --augment_batch_size "$augment_batch_size" \
+    --lr "$lr" \
+    --epochs "$epochs" \
+    --eval_interval "$eval_interval" \
+    --early_stop "$early_stop" \
+    --save_model "$save_model" \
+    --seed "$seed" \
+    --output_root_path "$output_root_path" \
+    --config_path "$config_path"
+
+  prefix="Data-${dataset_name}-${tr}-${ars}-${an}-${max_sample_num_per_user}_Model-${lightgcn_layers}-${noise_schedule}-${noise_scale}-${min_noise}-${max_noise}-${max_diffusion_steps}-${embedding_dim}"
+fi
+
+cd ./baselines
+
+cd ../BGCN
+python main.py -d "$dataset_name" --prefix "$prefix"
